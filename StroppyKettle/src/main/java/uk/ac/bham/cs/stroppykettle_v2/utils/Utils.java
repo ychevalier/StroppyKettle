f93f793297df9918fdf43ec68e9dd4451a63fc46
ychevalier <yann.m.chevalier@gmail.com>
2013-07-18 15:55:58 +0100

    Added remote server URL

--- StroppyKettle/src/main/java/uk/ac/bham/cs/stroppykettle_v2/utils/Utils.java
+++ StroppyKettle/src/main/java/uk/ac/bham/cs/stroppykettle_v2/utils/Utils.java
@@ -2,7 +2,11 @@ package uk.ac.bham.cs.stroppykettle_v2.utils;
 
 public class Utils {
 
-	public static final String SERVER_URL = "http://192.168.1.7:8080/stroppyserver";
+	// Local Server
+	//public static final String SERVER_URL = "http://192.168.1.7:8080/stroppyserver";
+
+	// Remote Server
+	public static final String SERVER_URL = "http://studentweb.cs.bham.ac.uk/yxc238/stroppyserver";
 
 	public static final String LOGS_PATH = "/logs_receiver";
 	public static final String INTERACTIONS_PATH = "/interactions_receiver";
